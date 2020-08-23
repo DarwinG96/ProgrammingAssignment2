07a0053974c1603bde210b03905faf683764af88
DarwinG96 <69228131+DarwinG96@users.noreply.github.com>
2020-08-22 22:49:08 -0500

    primer adicion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hello
+## I created 2 function;
 
-## Write a short comment describing this function
+## 1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## This function builds a set of functions and returns the functions within a list to the parent environment 
+##  It also includes the two data objects: 'data' and 'inv'.
 
-makeCacheMatrix <- function(x = matrix()) {
-hj
+makeCacheMatrix <- function(data = matrix()) {  # The input argument  'data' is a "matrix" object
+  inv <- NULL                                   # It starts with an empty object named 'inv'
+  setd <- function(x) {                         # In this function start two objects: 'data' and 'inv'
+    data <<- x                                  # these objects are cached
+    inv <<- NULL
+  }
+  getd <- function() data                       # This function defines the getter for the vector 'data'.
+  setinv<- function(inv2) inv <<- inv2          # This function defines the setter for the INVERSE of matrix in 'inv'
+  getinv <- function() inv                      # This function defines the getter for the INVERSE 'inv'
+  list(setd = setd, getd = getd,                # Create a new object by returning a list(), gives the name 'setd' to the setd() and 'getd' to the getd() functions 
+       setinv = setinv,                         # gives the name 'setinv' to the setinv() function defined above
+       getinv = getinv)                         # gives the name 'getinv' to the geinv() function defined above
 }
+ 
 
+## 2. 2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated, then the cachesolve retrieve the inverse from the cache.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(data, ...) {                     # Starts with a single argument 'data', and an ellipsis
+  inv <- data$getinv()                                  # Here retrieve the Inverse 'inv' from the object passed
+  if(!is.null(inv)) {                                   # Then it checks to see whether the result is NULL
+    message("Retrieving the inverse from the cache!")   # If this is TRUE, show message "Retrieving the inverse from the cache!"
+    return(inv)                                         # And it gets the INVERSE from the cache and return
+  }                                                     # if the condition is FALSE,
+  forsolve <- data$getd()                               # gets the 'data' from the input object
+  inv <- solve(forsolve, ...)                           # and calculates the INVERSE 
+  data$setinv(inv)                                      # then, uses the setinv() function defined above to set the INVERSE in the object 'inv'
+  inv                                                   # Print a matrix that is the INVERSE of 'data'
 }
